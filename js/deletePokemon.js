e62dc910ee04c22a66a963ebfd24dfb4bf53dc68
Nicolás Gómez García <nicogomezgarcia1@gmail.com>
2024-01-11 16:59:52 +0100

    modal para la eliminacion del pokemon con el boton eliminar con confirmacion

--- js/deletePokemon.js
+++ js/deletePokemon.js
@@ -1,9 +1,36 @@
 "use strict";
+document.addEventListener("DOMContentLoaded", function () {
+    let eliminarbtn = document.querySelectorAll(".eliminar-btn");
+
+    eliminarbtn.forEach(function (btn) {
+        btn.addEventListener("click", function () {
+            let id = btn.getAttribute("data-id");
+            eliminarPokemon(id);
+        });
+    });
+});
 function eliminarPokemon(id) {
-    if (confirm("¿Estás seguro que quieres eliminar?")) {
-        // Si el usuario hace clic en "Sí", muestra un mensaje de éxito 
-        alert("El Pokémon ha sido eliminado correctamente.");
-    } else {
-        alert("Se cancelo el proceso.");
-    }
+    // Obtiene el modal
+    let modal = document.getElementById("confirmModal");
+
+    // Muestra el modal
+    modal.style.display = "block";
+
+    // Obtiene los elementos de los botones en el modal
+    let btnConfirmar = document.getElementById("btnConfirmar");
+    let btnCancelar = document.getElementById("btnCancelar");
+
+    //boton de confirmar
+    btnConfirmar.onclick = function() {
+        // Oculta el modal
+        modal.style.display = "none";
+        console.log("El Pokémon con ID " + id + " ha sido eliminado correctamente.");
+    };
+
+    //boton de cancelar
+    btnCancelar.onclick = function() {
+        // Oculta el modal
+        modal.style.display = "none";
+        console.log("Se canceló el proceso de eliminación del Pokémon  con ID " + id);
+    };
 }
